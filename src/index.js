97b0178e339ffeec7de4a86156896015958762ff
allen <eepson123@gmail.com>
2021-08-09 00:42:32 +0800

    all

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './assets/css/index.css'
+
+const swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 5,
+  spaceBetween: 30,
+});
